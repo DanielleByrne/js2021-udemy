ed4fc5264fd03215d5b82062e4e62ef75de3e66e
Danielle Byrne <DanielleByrne421@gmail.com>
2021-01-13 14:13:31 -0500

    finishing section 3 of fundamentals review assignment

--- assignment.js
+++ assignment.js
@@ -1,15 +1,15 @@
 //LECTURE: values and variables 
 
 // console.log('hi')
-let country = "united states";
-let continent = "north america";
+const country = "united states";
+const continent = "north america";
 let population = "too many people";
 
 // console.log(country, continent, population)
 
 //LECTURE: data types
 
-let isIsland = false;
-let language; 
+const isIsland = false;
+const language = "english"; 
 
 console.log(isIsland, population, country, language)
