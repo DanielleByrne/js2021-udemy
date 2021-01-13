73a5e1367610db5b20b071f1b35c7ff547a62241
Danielle Byrne <DanielleByrne421@gmail.com>
2021-01-13 15:23:16 -0500

    finishing fundamentals review section 3 assignment

--- assignment.js
+++ assignment.js
@@ -3,7 +3,7 @@
 // console.log('hi')
 const country = "united states";
 const continent = "north america";
-let population = "too many people";
+let population = 328200000000;
 
 // console.log(country, continent, population)
 
@@ -12,4 +12,14 @@ let population = "too many people";
 const isIsland = false;
 const language = "english"; 
 
-console.log(isIsland, population, country, language)
+// console.log(isIsland, population, country, language)
+
+//LECTURE: basic operators
+population ++
+console.log(population)
+const finladPopulation = 6000000000
+const popDifference = population >= finladPopulation
+console.log(popDifference)
+console.log(population < 33000000000)
+
+console.log( country + " is in " + continent + ", and it's " + population + " people speak " + language)
--- section1.js
+++ section1.js
@@ -4,22 +4,33 @@
 // console.log(firstName);
 
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
 // console.log(typeof true);
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof "dani");
 
-javascriptIsFun = "yeah"
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "yeah"
+// console.log(typeof javascriptIsFun);
 
 
-let year;
-console.log(typeof year);
+// let year;
+// console.log(typeof year);
 
-year = 1995;
-console.log(typeof year);
+// year = 1995;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+const now = 2037
+const ageDani = now - 1995;
+const ageBrandon = now - 1993;
+console.log(ageDani, ageBrandon)
+
+const firstName = "Danielle";
+const lastName = "Byrne"; 
+console.log(firstName + " " + lastName)
+
+const isFullAge = ageDani >= 18;
+console.log(isFullAge)
