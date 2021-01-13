149b06c7e5d038f0d51fec5b11ce1be2b1312725
Danielle Byrne <DanielleByrne421@gmail.com>
2021-01-13 13:59:20 -0500

    finishing second part of fundaentals review

--- assignment.js
+++ assignment.js
@@ -1,7 +1,15 @@
-console.log('hi')
+//LECTURE: values and variables 
 
+// console.log('hi')
 let country = "united states";
 let continent = "north america";
-let populations = "too many people";
+let population = "too many people";
 
-console.log(country, continent, populations)
+// console.log(country, continent, population)
+
+//LECTURE: data types
+
+let isIsland = false;
+let language; 
+
+console.log(isIsland, population, country, language)
--- section1.js
+++ section1.js
@@ -1,4 +1,25 @@
-console.log(40 + 8 + 23 + 10);
+// console.log(40 + 8 + 23 + 10);
 
-let firstName = "jonas";
-console.log(firstName);
+// let firstName = "jonas";
+// console.log(firstName);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "dani");
+
+javascriptIsFun = "yeah"
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(typeof year);
+
+year = 1995;
+console.log(typeof year);
+
+console.log(typeof null);
